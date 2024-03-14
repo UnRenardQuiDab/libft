c91748af6f70f7bff9f1a8f823c0f1210f639e8c
Bastien Wisniewski <wisniewski.bastien31@gmail.com>
2024-03-14 18:44:30 +0100

    Add ft_strcmp

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: bwisniew <bwisniew@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:54:44 by bwisniew          #+#    #+#             */
-/*   Updated: 2024/01/18 15:06:54 by bwisniew         ###   ########.fr       */
+/*   Updated: 2024/03/14 18:43:49 by bwisniew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(char *s1, char *s2);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *nptr);
 int		ft_lstsize(t_list *lst);
